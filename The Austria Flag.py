c4bb0032e8d783ff2aeadc20848b07d5b1f566df
Astro-Princess <101041528+Astro-Princess@users.noreply.github.com>
2022-04-06 16:51:19 +0600

    Add files via upload

---
+++ The Austria Flag.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+speed(0)
+setup(800, 500)
+bgcolor("#ED2939")
+
+penup()
+goto(-400, -80)
+pendown()
+
+color("white")
+begin_fill()
+forward(800)
+left(90)
+forward(167)
+left(90)
+forward(800)
+end_fill()
+
+hideturtle()
